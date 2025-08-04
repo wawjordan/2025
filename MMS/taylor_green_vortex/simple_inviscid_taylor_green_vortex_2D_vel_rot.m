b6f5c2ef52e1af88ce4093fb9326a553d26ce721
Will <wajordan@vt.edu>
2025-08-04 19:24:19 -0400

    fixed mapping functions for rotating the solution (I think)

--- MMS/taylor_green_vortex/simple_inviscid_taylor_green_vortex_2D_vel_rot.m
+++ MMS/taylor_green_vortex/simple_inviscid_taylor_green_vortex_2D_vel_rot.m
@@ -1,10 +1,9 @@
 function [rho,u,v,p] = simple_inviscid_taylor_green_vortex_2D_vel_rot(x,y,L,xo,yo,theta)
 
-mapx = @(x,y)   x * cos(-theta) - y * sin(-theta);
-mapy = @(x,y)   x * sin(-theta) + y * cos(-theta);
-
 % mapx = @(x,~)   x;
 % mapy = @(~,y)   y;
+mapx = @(x,y)   x * cos(theta) + y * sin(theta);
+mapy = @(x,y)  -x * sin(theta) + y * cos(theta);
 
 
 [rho,u1,v1,p] = simple_inviscid_taylor_green_vortex_2D_vel(mapx(x,y),mapy(x,y),L,xo,yo);
